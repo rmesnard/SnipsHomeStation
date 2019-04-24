6ea388f41987be1be785fd058492d9d68ff2a161
rmesnard <gruno@free.fr>
2019-04-24 16:12:59 +0200

    Update start-snips.sh

--- start-snips.sh
+++ start-snips.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -e
 
-#verify that environment variables have been passed to this container. set the default value if not.
-ENABLE_MQTT=${ENABLE_MQTT:-no}
-ENABLE_HOTWORD_SERVICE=${ENABLE_HOTWORD_SERVICE:-yes}
 ENABLE_INTERCOM=${ENABLE_INTERCOM:-yes}
-
-if [ -d "/usr/share/snips/mbrola" ]; then
-	echo "Install mbrola voices:"
-	ls /usr/share/snips/mbrola
-	cp -r /usr/share/snips/mbrola /usr/share/mbrola
-	
-	cp -f mbrola /usr/bin/mbrola 
-	chmod 777 /usr/bin/mbrola
-fi
 
 echo "Install config."
 if [ ! -d "/usr/share/snips/config" ]; then
@@ -26,14 +14,6 @@ chmod -R 777 /usr/share/snips/config
 rm -f /etc/asound.conf
 cp -f /usr/share/snips/config/asound.conf /etc/asound.conf
 
-echo "Install assistant."
-if [ ! -d "/usr/share/snips/assistant" ]; then
-  echo "Install default assistant."
-  mkdir /usr/share/snips/assistant
-  cp -R -f /assistant /usr/share/snips
-fi
-chmod -R 777 /usr/share/snips/assistant
-
 
 echo "Install skills."
 if [ ! -d "/usr/share/snips/skills" ]; then
@@ -42,10 +22,6 @@ if [ ! -d "/usr/share/snips/skills" ]; then
   cp -R -f /skills /usr/share/snips
 fi
 chmod -R 777 /usr/share/snips/skills
-
-echo "Deploy assistant."
-
-#goto skill directory
 
 if [ -d /usr/share/snips/config ]; then
 	echo "use shared config."
@@ -57,42 +33,9 @@ fi
 #go back to root directory
 cd /
 
-#start own mqtt service.
-if [ $ENABLE_MQTT == yes ]; then
-	echo "Start mosquitto"
-	mosquitto -d
-fi
-
 echo "Start snips services"
 
 chmod -R 777 /var/log
-
-#start Snips analytics service
-snips-analytics 2> /var/log/snips-analytics.log  &
-snips_analytics_pid=$!
-
-#start Snips' Automatic Speech Recognition service
-snips-asr 2> /var/log/snips-asr.log &
-snips_asr_pid=$!
-
-#start Snips-dialogue service
-snips-dialogue 2> /var/log/snips-dialogue.log  &
-snips_dialogue_pid=$!
-
-#start Snips hotword service
-
-if [ $ENABLE_HOTWORD_SERVICE == yes ]; then
-	snips-hotword 2> /var/log/snips-hotword.log & 
-	snips_hotword_pid=$!
- fi
-
-#start Snips Natural Language Understanding service
-snips-nlu 2> /var/log/snips-nlu.log &
-snips_nlu_pid=$!
-
-#start Snips TTS service
-snips-tts 2> /var/log/snips-tts.log &
-snips_tts_pid=$!
 
 #start the snips audio server 
 snips-audio-server --hijack localhost:64321 2> /var/log/snips-audio-server.log &
@@ -115,5 +58,5 @@ snips_homestation_pid=$!
 
 echo "running ok"
 
-wait "$snips_analytics_pid" "$snips_asr_pid" "$snips_dialogue_pid" "$snips_hotword_pid" "$snips_nlu_pid" "$snips_tts_pid" "$snips_audio_server_pid"
+wait "$snips_audio_server_pid"
 
